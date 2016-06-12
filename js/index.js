2d5d97aa3b6adeeb4548a211adb4444f82a643cd
Shawn Conners <shawniconners@icloud.com>
2016-06-12 13:45:56 -0500

    Initial Upload of Files

---
+++ js/index.js
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener('touchmove', preventBehavior, false);
+		//document.getElementById("div_start_button").addEventListener('touchstart', function(){funStartGame();}, false);
+		//document.getElementById("div_home_button").addEventListener('touchstart', function(){funHome();}, false);
+		//document.getElementById("div_quit_button").addEventListener('touchstart', function(){funHome();}, false);
+		//document.getElementById("div_learn_button").addEventListener('touchstart', function(){funLearn();}, false);
+		
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        //app.receivedEvent('deviceready');
+		funStart();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        /*
+		 var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+		 */
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+
+
